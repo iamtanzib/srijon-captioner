50a9fa080cd04df1fed71053b3612901041044ef
Tanzib Sajib <tanzibhere@gmail.com>
2026-09-25 05:13:25 +0600

    Install Stylize bridge with development builds

--- setup/BUILD_AND_INSTALL.bat
+++ setup/BUILD_AND_INSTALL.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set "VER=%~1"
-if "%VER%"=="" set "VER=1.2.14"
+if "%VER%"=="" set "VER=1.3.0"
 call "%~dp0BUILD_CCX.bat" --version %VER% --name Srijon-Captioner-v%VER%-DevBuild.ccx
 if errorlevel 1 exit /b %errorlevel%
 set "UPIA=C:\Program Files\Common Files\Adobe\Adobe Desktop Common\RemoteComponents\UPI\UnifiedPluginInstallerAgent\UnifiedPluginInstallerAgent.exe"
@@ -12,5 +12,7 @@ if not exist "%UPIA%" (
 )
 "%UPIA%" /remove "Srijon Captioner"
 "%UPIA%" /install "%~dp0..\build\Srijon-Captioner-v%VER%-DevBuild.ccx"
+if errorlevel 1 exit /b %errorlevel%
+call "%~dp0INSTALL_STYLIZE_BRIDGE.bat"
 "%UPIA%" /list all
 pause
